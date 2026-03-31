9f21357dbe7e326a798b46d88740f0479a405e66
implefy <implefy@users.noreply.github.com>
2026-03-31 16:39:23 +0200

    Bump version to 19.0.1.0.1

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Pricelist Handling',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'depends': ['website_sale'],
     'data': [
         'security/ir.model.access.csv',
